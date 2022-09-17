e84c8b1bfc0c4a15458151750d16cdeebd121b77
Emil Flach <emil.flach@skopei.com>
2022-09-17 12:56:45 +0200

    changed default config to emil's

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-window.documentTitle = "Paul & Dana's plants";
-window.pageTitle = "Paul & Dana's";
-window.googleSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFRg12tuT66exAX2F3Y-jXmbdHo-I4Qgxtr9gDGqexbMVCJI_Ax0jSkkpxbiz7blUsQBpEKYvZvgPj/pub?gid=0&single=true&output=csv";
+window.documentTitle = "Emil & Lucia's plants";
+window.pageTitle = "Emil & Lucia's";
+window.googleSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQe6xtyzDqTqHQW3Zflfa5i0XmQbMv_jWjeKIe5vA-5VPY5wLjFIvVukCdIXjyxwhHXb5Jul8pmTA3B/pub?gid=0&single=true&output=csv";
